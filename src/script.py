a1f75de60a4f97554966fd13ba9b9e23bc6d6003
arcweld <smweld.af@gmail.com>
2021-06-09 15:30:24 -0400

    Wed after lunch

--- src/script.py
+++ src/script.py
@@ -1,17 +1,8 @@
-import matplotlib.pyplot as plt
-
-def myplot(f, x):
-    """
-    :param f: function to plot
-    :type f: callable
-    :param x: values for x
-    :type x: list or ndarray
-
-    Plots the function f(x).
-    """
-    # yes, you can pass functions around as if
-    # they were ordinary variables (they are)
-    plt.plot(x, f(x))
-    plt.xlabel("Eje $x$",fontsize=16)
-    plt.ylabel("$f(x)$",fontsize=16)
-    plt.title("Funcion $f(x)$")
+def welch_satterhwaithe_dof(sample_1, sample_2):
+    ss1 = len(sample_1)
+    ss2 = len(sample_2)
+    dof = (
+        ((np.var(sample_1)/ss1 + np.var(sample_2)/ss2)**(2.0)) / 
+        ((np.var(sample_1)/ss1)**(2.0)/(ss1 - 1) + (np.var(sample_2)/ss2)**(2.0)/(ss2 - 1))
+    )
+    return dof
